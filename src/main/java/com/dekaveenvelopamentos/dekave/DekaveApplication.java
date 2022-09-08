c01ca8bd49430e0d8837186f406f5dce93a1a64c
[stefanovezzoni] <[stefanov.santos@gmail.com]>
2022-09-08 08:31:52 -0300

    chore (DekaveApplication) added OpenApiDefinition.

--- src/main/java/com/dekaveenvelopamentos/dekave/DekaveApplication.java
+++ src/main/java/com/dekaveenvelopamentos/dekave/DekaveApplication.java
@@ -3,6 +3,10 @@ package com.dekaveenvelopamentos.dekave;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(info = @Info(title = "Dekave API", version = "1.0"))
 @SpringBootApplication
 public class DekaveApplication {
 
