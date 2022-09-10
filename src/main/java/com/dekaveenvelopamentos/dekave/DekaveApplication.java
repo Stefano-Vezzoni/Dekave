0e91ccdfa4b0cf713039cf6436234afa171a9285
[stefanovezzoni] <[stefanov.santos@gmail.com]>
2022-09-10 13:06:15 -0300

    feature (DekaveApplication) added CORS map

--- src/main/java/com/dekaveenvelopamentos/dekave/DekaveApplication.java
+++ src/main/java/com/dekaveenvelopamentos/dekave/DekaveApplication.java
@@ -2,6 +2,9 @@ package com.dekaveenvelopamentos.dekave;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
@@ -12,6 +15,16 @@ public class DekaveApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DekaveApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
 	}
 
 }
